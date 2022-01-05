95774f126ee18c64a592320e5bb3684b20cb4643
YangJinwoo <86765716+YangNaang2@users.noreply.github.com>
2022-01-05 13:50:28 +0900

    Add files via upload

---
+++ 1~10번.py
@@ -0,0 +1,21 @@
+#1
+print("Hello World")
+#2
+print("Mary's cosmetics")
+#3
+print('신씨가 소리질렀다. "도둑이야".')
+#4
+print("C:\Windows")
+#5
+## \n은 줄을 띄우고 \t은 tab이다.
+#6
+print(' "오늘은", "일요일" ')
+#7
+print("naver", "kakao", "samsung", sep=";")
+##sep인자로 ; 입력하면 출력 값들 사이에 ; 출력한다.
+#8
+print("naver", "kakao", "sk", "samsung", sep = "/")
+#9
+print("first", end=""); print("second")
+#10
+print(5/3)

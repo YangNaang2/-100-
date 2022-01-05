e2b6e374e0e671026c584c94f69be4be6441c3ee
YangJinwoo <86765716+YangNaang2@users.noreply.github.com>
2022-01-05 14:25:23 +0900

    Add files via upload

---
+++ 11~20번.py
@@ -0,0 +1,41 @@
+#11
+삼성전자 = 50000
+총평가금액 = 삼성전자 * 10
+print(총평가금액)
+#12
+시가총액 = 298000000000000
+현재가 = 5000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+#13
+s = "hello"
+t = "python"
+print(s+ "! " + t)
+#14
+print(2+2*3)
+#15
+a= "132"
+print (type(a))
+#16
+num_str = "720"
+num_int = int(num_str)
+print(num_int, type(num_int))
+#17
+num = 100
+num_str = str(num)
+print(num_str, type(num_str))
+#18
+num = "15.79"
+num=float(num)
+print(num, type(num))
+#19
+year = "2020"
+year = int(year)
+print(year)
+print(year -1)
+print(year -2)
+#20
+a= 48854 * 36
+print(a)

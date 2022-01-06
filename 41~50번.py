d1281fe66897346944027194043aad57fbe644b7
YangJinwoo <86765716+YangNaang2@users.noreply.github.com>
2022-01-06 16:08:04 +0900

    Add files via upload

---
+++ 41~50번.py
@@ -0,0 +1,47 @@
+print("\n41번")
+ticker = "btc_krw"
+a= ticker.upper()
+print(a)
+
+print("\n42번")
+ticker = "BTC_KRW"
+a = ticker.lower()
+print(a)
+
+print("\n43번")
+a="hello"
+b= a.capitalize()
+print(b)
+
+print("\n44번")
+file_name = "보고서.xlxs"
+file_name.endswith('xlxs')
+
+print("\n45번")
+file_name = "보고서.xlsx"
+file_name.endswith(("xlsx", "xls"))
+
+print("\n46번")
+file_name = "2020_보고서.xlsx"
+file_name .startswith("2020")
+
+print("\n47번")
+a = "hello world"
+a.split( )
+print(a)
+
+print("\n48번")
+ticker = "btc_krw"
+ticker.split("_")
+print(ticker)
+
+print("\n49번")
+date = "2020-05-01"
+date.split("-")
+print(date)
+
+print("\n50번")
+data = "03490         "
+data = data.rstrip( )
+print(data)
+

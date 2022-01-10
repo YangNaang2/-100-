d4b959d41fdac571e89cc54f18c6915b12a3642d
YangJinwoo <86765716+YangNaang2@users.noreply.github.com>
2022-01-10 14:39:15 +0900

    Add files via upload

---
+++ 51~60번.py
@@ -0,0 +1,49 @@
+print("\n51번")
+movie_rank = ["닥터 스트레인지", "스플릿", "럭키"]
+print(movie_rank)
+
+print("\n52번")
+movie_rank = ["닥터 스트레인지", "스플릿", "럭키"]
+movie_rank.append("베트맨")
+print(movie_rank)
+
+print("\n53번")
+movie_rank = ['닥터 스트레인지', '스플릿', '럭키', '배트맨']
+movie_rank.insert(1, "슈퍼맨")
+print(movie_rank)
+
+print("\n54번")
+movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '럭카',  '배트맨']
+del movie_rank[3]
+print(movie_rank)
+
+print("\n55번")
+movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '배트맨']
+del movie_rank[3]
+del movie_rank[2]
+print(movie_rank)
+
+print("\n56번")
+lang1 = ["C", "C++", "JAVA"]
+lang2 = ["Python", "Go", "C#"]
+langs = lang1 + lang2
+print(langs)
+
+print("\n57번")
+nums = [1,2,3,4,5,6,7]
+print("max: ", max(nums))
+print("min: ", min(nums))
+
+print("\n58번")
+nums = [1,2,3,4,5]
+print(sum(nums))
+
+print("\n59번")
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+print(len(cook))
+
+print("\n60번")
+nums = [1,2,3,4,5]
+avg = sum(nums)/len(nums)
+print(avg)
+
